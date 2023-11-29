7f0baa1871f2531bc11b970f75eee94653e4e40a
nafef60552 <nafef60552@qianhost.com>
2023-11-29 13:54:46 +0200

    changed from "letter for us" to "letter to us"

--- express/no-db/src/constant/error-messages.ts
+++ express/no-db/src/constant/error-messages.ts
@@ -4,7 +4,7 @@ export class ErrorMessages {
   public static readonly ALREADY_HAVE_AN_ACC = "Already have an account";
   public static readonly DONT_HAVE_SUCH_ACC = "Dont have such account";
   public static readonly SERVER_ERROR =
-    "Something went bad, please describe your situation in letter for us";
+    "Something went bad, please describe your situation in letter to us";
   public static readonly UNAUTHORIZED = "Unauthorized";
   public static readonly WRONG_PASS = "Wrong password";
   public static readonly DATA_NOT_FOUND = "Data not found";
