25c459ed811d7923e4873976fc429c1b97444d3c
nafef60552 <nafef60552@qianhost.com>
2023-11-29 13:55:06 +0200

    removed useless .end()

--- express/no-db/src/middleware/error-handler-middleware.ts
+++ express/no-db/src/middleware/error-handler-middleware.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request, Response } from 'express';
-import { ErrorCodes } from '../constant/error-codes';
-import { ErrorMessages } from '../constant/error-messages';
-import { AppError } from '../entity/error/app-error';
+import { NextFunction, Request, Response } from "express";
+import { ErrorCodes } from "../constant/error-codes";
+import { ErrorMessages } from "../constant/error-messages";
+import { AppError } from "../entity/error/app-error";
 
 export function errorHandlerMiddleware(
   err: Error,
@@ -11,14 +11,11 @@ export function errorHandlerMiddleware(
 ) {
   if (err instanceof AppError) {
     const httpStatus = Number(err.code.toString().slice(0, 3));
-    return res
-      .status(httpStatus)
-      .json({
-        code: err.code,
-        message: err.message,
-        errorStack: err.errorStack,
-      })
-      .end();
+    return res.status(httpStatus).json({
+      code: err.code,
+      message: err.message,
+      errorStack: err.errorStack,
+    });
   }
   return res.status(500).json({
     code: ErrorCodes.SERVER_ERROR,
