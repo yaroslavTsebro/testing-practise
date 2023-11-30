91956669d098068e5bcc6a9d38a14fc99b7226d1
nafef60552 <nafef60552@qianhost.com>
2023-11-30 18:27:48 +0200

    added tests

--- express/in-memory/__tests__/unit/repository/user-repository.test.ts
+++ express/in-memory/__tests__/unit/repository/user-repository.test.ts
@@ -1,82 +1,83 @@
-import "reflect-metadata";
-import { mock } from "jest-mock-extended";
-import { Repository, SelectQueryBuilder, getRepository } from "typeorm";
+import { faker } from "@faker-js/faker";
+import userRepository from "../../../src/repository/user-repository";
+import connectToDB from "../__helpers__/setup-db";
+import { createConnection } from "typeorm";
 import { User } from "../../../src/entity/db/model/user";
-import UserRepository from "../../../src/repository/user-repository";
-
-jest.mock('typeorm', () => {
-  const actual = jest.requireActual('typeorm');
-  return {
-    ...actual,
-    getCustomRepository: jest.fn(),
-  }
-});
-
-const mockRepository = mock<Repository<User>>();
 
 describe("UserRepository", () => {
 
-  describe("findById", () => {
-    it("should find a user by their ID", async () => {
-      const mockUser = new User();
-      mockUser.id = "123";
-      mockRepository.findOne.mockResolvedValue(mockUser);
+  // beforeAll( async () => {
+    
+  // });
 
-      const result = await UserRepository.findById(mockUser.id);
 
-      expect(mockRepository.findOne).toHaveBeenCalledWith({
-        where: { id: mockUser.id },
-      });
-      expect(result).toBe(mockUser);
-    });
-  });
+  // describe("findById", () => {
+  //   it("should find a user by their ID", async () => {
+  //     const mockUser = new User();
+  //     mockUser.id = "123";
+  //     mockRepository.findOne.mockResolvedValue(mockUser);
 
-  describe("findByEmail", () => {
-    it("should find a user by their email", async () => {
-      const mockUser = new User();
-      mockUser.email = "test@example.com";
-      mockRepository.findOne.mockResolvedValue(mockUser);
+  //     const result = await UserRepository.findById(mockUser.id);
 
-      const result = await UserRepository.findByEmail("test@example.com");
+  //     expect(mockRepository.findOne).toHaveBeenCalledWith({
+  //       where: { id: mockUser.id },
+  //     });
+  //     expect(result).toBe(mockUser);
+  //   });
+  // });
 
-      expect(mockRepository.findOne).toHaveBeenCalledWith({
-        where: { email: "test@example.com" },
-      });
-      expect(result).toBe(mockUser);
-    });
-  });
+  // describe("findByEmail", () => {
+  //   it("should find a user by their email", async () => {
+  //     const mockUser = new User();
+  //     mockUser.email = "test@example.com";
+  //     mockRepository.findOne.mockResolvedValue(mockUser);
 
-  describe("deleteById", () => {
-    it("should soft delete a user by ID", async () => {
-      await UserRepository.deleteById("123");
-      expect(mockRepository.softDelete).toHaveBeenCalledWith("123");
-    });
-  });
+  //     const result = await UserRepository.findByEmail("test@example.com");
 
-  describe("update", () => {
-    it("should update a user by their ID", async () => {
-      const dto = { email: "newnew@gmail.com" };
-      await UserRepository.update(dto, "123");
+  //     expect(mockRepository.findOne).toHaveBeenCalledWith({
+  //       where: { email: "test@example.com" },
+  //     });
+  //     expect(result).toBe(mockUser);
+  //   });
+  // });
 
-      expect(mockRepository.update).toHaveBeenCalledWith("123", dto);
-    });
-  });
+  // describe("deleteById", () => {
+  //   it("should soft delete a user by ID", async () => {
+  //     await UserRepository.deleteById("123");
+  //     expect(mockRepository.softDelete).toHaveBeenCalledWith("123");
+  //   });
+  // });
+
+  // describe("update", () => {
+  //   it("should update a user by their ID", async () => {
+  //     const dto = { email: "newnew@gmail.com" };
+  //     await UserRepository.update(dto, "123");
+
+  //     expect(mockRepository.update).toHaveBeenCalledWith("123", dto);
+  //   });
+  // });
 
   describe("create", () => {
     it("should create a user", async () => {
+      //Arrange
+      await createConnection({
+        type: "better-sqlite3",
+        database: ":memory:",
+        entities: [User],
+        logging: true,
+        synchronize: true,
+      });
       const dto = {
-        password: "ferfrfrefdwed32201p,",
-        email: "john@example.com",
+        password: faker.internet.password(),
+        email: faker.internet.email(),
       };
-      const mockUser = new User();
-      mockRepository.create.mockReturnValue(mockUser);
-      mockRepository.save.mockResolvedValue(mockUser);
 
-      const result = await UserRepository.create(dto);
+      //Act
+      const user = await userRepository.create(dto);
 
-      expect(mockRepository.create).toHaveBeenCalledWith(dto);
-      expect(mockRepository.save).toHaveBeenCalledWith(mockUser);
-      expect(result).toBe(mockUser);
+      //Assert
+      expect(user.email).toEqual(dto.email);
+      expect(user.password).toEqual(dto.password);
     });
   });
 });
